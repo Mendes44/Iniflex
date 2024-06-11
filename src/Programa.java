5fd813ccc554b4fe9337b0af95289cfe6dd5d82f
Mendes44 <marcosmendesm10@gmail.com>
2024-06-11 12:57:28 -0300

    Criando Classe Pessoas e Programa

---
+++ src/Programa.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("teste");
+		double x = sc.nextDouble();
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
